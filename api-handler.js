56f0047ae3e58c2ce8326730327e5ac64f13818c
Poschi003 <c.poschenrieder@outlook.de>
2026-05-02 16:49:22 +0200

    Add files via upload

---
+++ api-handler.js
@@ -0,0 +1,14 @@
+const { handleApi } = require("../lib/api-handler");
+
+module.exports = async function handler(req, res) {
+  try {
+    await handleApi(req, res);
+  } catch (error) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    res.end(JSON.stringify({
+      error: error.message || "Serverfehler",
+      hint: "Pruefe in Vercel die Environment Variables und ob Supabase die Tabelle app_state hat."
+    }));
+  }
+};
